8dd9787f8a0d0afcc8979f70100c76be33d9d1a1
Michael Chow <machow@princeton.edu>
2017-12-19 13:47:55 -0500

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,3 +1,3 @@
-devtools::install_version("raster", "2.6-7")
+devtools::install_github("machow/raster@7ff620739e271b334ee118d9fb7fa199d8d5b214")
 devtools::install_version("rgdal", "1.2-16")
 devtools::install_version("sf", "0.5-4")
